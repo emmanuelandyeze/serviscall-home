abbd7ec25e0d620295e2b26a3ac257e2d2027a57
emmanuelandyeze <emmanuel.andyeze@gmail.com>
2024-09-25 13:37:16 +0100

    navbar and hero-section

---
+++ app/components/Hero.js
@@ -0,0 +1,133 @@
+// app/components/Hero.js
+import { IoHomeOutline } from 'react-icons/io5';
+import { HiOutlineLightningBolt } from 'react-icons/hi';
+import { GiWoodBeam } from 'react-icons/gi';
+import { MdOutlineCleaningServices } from 'react-icons/md';
+import { GiPaintRoller } from 'react-icons/gi';
+import { GiMetalBar } from 'react-icons/gi';
+import { GiFloorPolisher } from 'react-icons/gi';
+import { TbTools } from 'react-icons/tb';
+
+const services = [
+	{
+		title: 'Home Maintenance',
+		image: '/images/22.jpg',
+		icon: (
+			<IoHomeOutline className="text-5xl mb-3 text-yellow-400" />
+		),
+	},
+	{
+		title: 'Electrical Services',
+		image: '/images/electricity.png',
+		icon: (
+			<HiOutlineLightningBolt className="text-5xl mb-3 text-yellow-400" />
+		),
+	},
+	{
+		title: 'Carpentry',
+		image: '/images/12.jpg',
+		icon: (
+			<GiWoodBeam className="text-5xl mb-3 text-yellow-400" />
+		),
+	},
+	{
+		title: 'Cleaning',
+		image: '/images/cleaning.jpg',
+		icon: (
+			<MdOutlineCleaningServices className="text-5xl mb-3 text-yellow-400" />
+		),
+	},
+	{
+		title: 'Painting',
+		image: '/images/11.jpg',
+		icon: (
+			<GiPaintRoller className="text-5xl mb-3 text-yellow-400" />
+		),
+	},
+	{
+		title: 'Metal Works',
+		image: '/images/2.jpg',
+		icon: (
+			<GiMetalBar className="text-5xl mb-3 text-yellow-400" />
+		),
+	},
+	{
+		title: 'Flooring',
+		image: '/images/21.jpg',
+		icon: (
+			<GiFloorPolisher className="text-5xl mb-3 text-yellow-400" />
+		),
+	},
+	{
+		title: 'Project Management',
+		image: '/images/17.jpg',
+		icon: (
+			<TbTools className="text-5xl mb-3 text-yellow-400" />
+		),
+	},
+];
+
+const Hero = () => {
+	return (
+		<div
+			className="relative pt-40 bg-cover bg-center"
+			style={{
+				backgroundImage: "url('/images/17.jpg')",
+			}}
+		>
+			{/* Overlay */}
+			<div className="absolute inset-0 bg-[#C7234E] opacity-[0.95]"></div>
+
+			{/* Centered Text */}
+			<div className="relative flex flex-col justify-center items-center h-full text-white text-center">
+				<div>
+					<h1 className="text-4xl md:text-6xl font-bold">
+						Bring your{' '}
+						<span className="underline underline-offset-4">
+							ideas
+						</span>{' '}
+						to life with us!
+					</h1>
+					<p className="mt-4 text-xl md:text-2xl">
+						We offer a variety of services to meet your
+						needs.
+					</p>
+				</div>
+				{/* Services Grid */}
+				<div className="relative mt-8 lg:mt-16">
+					<div className="container max-w-6xl mx-auto px-4 py-8">
+						<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+							{services.map((service, index) => (
+								<div
+									key={index}
+									className="relative bg-cover w-80 md:w-64 bg-center h-[14rem] rounded-none shadow-lg"
+									style={{
+										backgroundImage: `url(${service.image})`,
+									}}
+								>
+									{/* Overlay */}
+									<div className="absolute inset-0 bg-black bg-opacity-[0.65] rounded-none"></div>
+
+									{/* Service Content */}
+									<div className="relative z-10 flex flex-col justify-center items-center h-full">
+										{/* <img
+											src={service.icon}
+											alt={`${service.title} icon`}
+											className="w-12 h-12 mb-4"
+										/> */}
+										{service.icon}
+										<h2 className="text-white text-2xl font-semibold">
+											{service.title}
+										</h2>
+									</div>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
---
+++ app/components/Navbar.js
@@ -0,0 +1,202 @@
+// app/components/Navbar.js
+'use client';
+import { useState } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const Navbar = () => {
+	const [menuOpen, setMenuOpen] = useState(false); // State to track if the mobile menu is open
+	const pathname = usePathname(); // Get the current pathname
+
+	// Helper function to check if a link is active
+	const isActive = (path) => pathname === path;
+
+	// Toggle mobile menu
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	};
+
+	return (
+		<nav className="bg-white fixed w-full shadow-sm top-0 left-0 z-50">
+			<div className="container max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
+				{/* Logo */}
+				<div className="">
+					<Link href="/">
+						<img
+							src="/images/logo.png"
+							className="h-12"
+							alt="Serviscall logo"
+						/>
+					</Link>
+				</div>
+
+				{/* Navigation Links (Desktop) */}
+				<div className="hidden md:flex items-center space-x-8 text-md">
+					<Link
+						href="/"
+						className={
+							isActive('/')
+								? 'text-[#ED0D0D] font-bold'
+								: 'hover:text-[#ED0D0D]'
+						}
+					>
+						Home
+					</Link>
+					<Link
+						href="/services"
+						className={
+							isActive('/services')
+								? 'text-[#ED0D0D] font-bold'
+								: 'hover:text-[#ED0D0D]'
+						}
+					>
+						Services
+					</Link>
+					<Link
+						href="/about-us"
+						className={
+							isActive('/about-us')
+								? 'text-[#ED0D0D] font-bold'
+								: 'hover:text-[#ED0D0D]'
+						}
+					>
+						About Us
+					</Link>
+					<Link
+						href="https://serviscall.com"
+						target="_blank"
+						className={
+							isActive('/faqs')
+								? 'text-[#ED0D0D] font-bold'
+								: 'hover:text-[#ED0D0D]'
+						}
+					>
+						Shop
+					</Link>
+					<Link
+						href="/contact-us"
+						className={
+							isActive('/contact-us')
+								? 'text-[#ED0D0D] font-bold'
+								: 'hover:text-[#ED0D0D]'
+						}
+					>
+						Contact Us
+					</Link>
+				</div>
+
+				{/* Mobile Menu Button */}
+				<div className="md:hidden mt-1">
+					<button
+						onClick={toggleMenu}
+						className="text-[#ED0D0D]"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth="2"
+							stroke="currentColor"
+							className="w-6 h-6"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M4 6h16M4 12h16m-7 6h7"
+							/>
+						</svg>
+					</button>
+				</div>
+			</div>
+
+			{/* Mobile Menu */}
+			<div
+				className={`fixed top-0 right-0 h-full w-2/3 bg-white shadow-lg z-40 transform transition-transform duration-300 ease-in-out ${
+					menuOpen ? 'translate-x-0' : 'translate-x-full'
+				}`}
+			>
+				<div className="p-4 flex justify-end">
+					<button
+						onClick={toggleMenu}
+						className="text-[#ED0D0D]"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth="2"
+							stroke="currentColor"
+							className="w-6 h-6"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M6 18L18 6M6 6l12 12"
+							/>
+						</svg>
+					</button>
+				</div>
+				<div className="flex flex-col items-center space-y-6 text-md p-8">
+					<Link
+						href="/"
+						onClick={toggleMenu}
+						className={
+							isActive('/')
+								? 'text-[#ED0D0D] font-bold'
+								: 'hover:text-[#ED0D0D]'
+						}
+					>
+						Home
+					</Link>
+					<Link
+						href="/services"
+						onClick={toggleMenu}
+						className={
+							isActive('/services')
+								? 'text-[#ED0D0D] font-bold'
+								: 'hover:text-[#ED0D0D]'
+						}
+					>
+						Services
+					</Link>
+					<Link
+						href="/about-us"
+						onClick={toggleMenu}
+						className={
+							isActive('/about-us')
+								? 'text-[#ED0D0D] font-bold'
+								: 'hover:text-[#ED0D0D]'
+						}
+					>
+						About Us
+					</Link>
+					<Link
+						href="https://serviscall.com"
+						target="_blank"
+						onClick={toggleMenu}
+						className={
+							isActive('/faqs')
+								? 'text-[#ED0D0D] font-bold'
+								: 'hover:text-[#ED0D0D]'
+						}
+					>
+						Shop
+					</Link>
+					<Link
+						href="/contact-us"
+						onClick={toggleMenu}
+						className={
+							isActive('/contact-us')
+								? 'text-[#ED0D0D] font-bold'
+								: 'hover:text-[#ED0D0D]'
+						}
+					>
+						Contact Us
+					</Link>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
